97122ebd6f5fdf00fdebdf7e1c928aae7fb4c80f
ZCube <zcube@zcube.kr>
2020-06-19 01:20:22 +0900

    pure_c = False

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(pure_c=False)
     builder.run()
