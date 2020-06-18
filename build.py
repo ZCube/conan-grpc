7bf92cb26d2bfab7e07d7acd4530f6b4de59a13f
ZCube <zcube@zcube.kr>
2020-06-19 01:44:17 +0900

    update build.py

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
-from cpt.packager import ConanMultiPackager
+#!/usr/bin/env python
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=False)
+    builder = build_template_default.get_builder(pure_c=False)
     builder.run()
